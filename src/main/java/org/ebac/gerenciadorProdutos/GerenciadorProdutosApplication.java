1cba707cca891f69a0e1f1b8e38650836c9a0745
joaoalbertorsc <jaoalbertorsc@gmail.com>
2026-07-01 14:21:38 -0300

    initialize Spring Boot application with basic configuration and dependencies

---
+++ src/main/java/org/ebac/gerenciadorProdutos/GerenciadorProdutosApplication.java
@@ -0,0 +1,12 @@
+package org.ebac.gerenciadorProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorProdutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorProdutosApplication.class, args);
+    }
+}
